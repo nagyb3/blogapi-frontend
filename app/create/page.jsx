6f699c37e3f74bf892cae6491a15be9b826312fc
nagyb3 <nagybence2003@gmail.com>
2023-07-16 13:23:23 +0200

    submit psot

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -6,6 +6,10 @@ import { Editor } from '@tinymce/tinymce-react';
 export default function Create() {
     
     const editorRef = useRef(null);
+
+    const [browserHasAccessToken, setBrowserHasAccessToken] = React.useState(false);
+
+    const [newTitleState, setNewTitleState] = React.useState("");
     
     const log = () => {
         if (editorRef.current) {
@@ -15,24 +19,27 @@ export default function Create() {
     
     React.useEffect(() => {
         
-    }, [])
+    }, [browserHasAccessToken])
 
     const submitPost = () => {
         fetch("http://localhost:5000/posts/create", { 
             method: "POST",
             headers: {
-                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                'Content-Type': 'application/json',
             },
-            body: {
-                'posttext': editorRef.current.getContent()
-            }
+            body: JSON.stringify({
+                'postcontent': editorRef.current.getContent(),
+                'title': newTitleState
+            })
         }).then((response) => {
+            console.log(response);
             return response;
         }).then(data => {
             console.log(data);
         }).catch(error => {
             console.error(error);
-        })
+        });
     };
 
     const login = () => {
@@ -41,17 +48,28 @@ export default function Create() {
         }).then(response => {
             return response.json();
         }).then(data => {
-            // console.log(data.token);
             localStorage.setItem('access_token', data.token)
+            setBrowserHasAccessToken(true);
         }).catch(error => {
             console.error(error)
         })
     }
 
+    const logout = () => {
+        localStorage.removeItem('access_token');
+        setBrowserHasAccessToken(false);
+    }
+
+    console.log(browserHasAccessToken)
+
     return (
-        <div className="flex flex-col items-center justify-center min-w-full p-10 gap-5">
+        <div className="min-h-screen flex flex-col items-center justify-center min-w-full p-10 gap-5">
             <h1 className="font-bold text-2xl">Make Blog Post</h1>
             <div className="max-w-7xl flex flex-col items-center justify-center gap-5">
+                <div>
+                    <label htmlFor="title">TITLE OF THE POST:</label>
+                    <input className="text-black" onChange={e => setNewTitleState(e.target.value)} value={newTitleState} type="text" name="title" id="title" />
+                </div>
                 <Editor
                     apiKey='9utnb2ang81zj7r55a0smpbengk80fx7utcnliw8bielweiy'
                     onInit={(evt, editor) => editorRef.current = editor}
@@ -71,9 +89,10 @@ export default function Create() {
                     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                     }}
                 />
-                <button className="m-2 p-3 bg-cyan-800 w-fit rounded-2xl font-bold" onClick={submitPost}>Log editor content</button>
-                <button onClick={login}>LOGIN</button>
+                <button className="text-white m-2 p-3 bg-cyan-800 w-fit rounded-2xl font-bold" onClick={submitPost}>SUBMIT POST</button>
             </div>
+            <button onClick={login}>LOGIN</button>
+            <button onClick={logout}>LOGOUT</button>
         </div>
     )
 }

45c6bb54329883d601be7d8210c6766e6cc05b18
nagyb3 <nagybence2003@gmail.com>
2023-07-16 11:45:46 +0200

    comment form

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -6,10 +6,14 @@ export default function Post(props) {
     
     const [postData, setPostData] = React.useState(null);
 
-    React.useEffect(() => {
+    const [newCommentEmail, setNewCommentEmail] = React.useState("");
+
+    const [newCommentText, setNewCommentText] = React.useState("");
+
+    const fetchData = async() => {
         const params = new URLSearchParams(window.location.search);
         const postId = params.get("postid");
-        console.log('postId');
+        // console.log('postId');
         fetch(`http://localhost:5000/posts/${postId}`)
             .then(response => {
                 return response.json();
@@ -19,7 +23,32 @@ export default function Post(props) {
             }).catch(error => {
                 console.error(error);
             })
+    }
+
+    React.useEffect(() => {
+        fetchData();
     }, []);
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setNewCommentEmail('');
+        setNewCommentText('');
+        await fetch("http://localhost:5000/comments/create", {
+            method: "POST", 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: newCommentEmail,
+                comment: newCommentText,
+                postid: postData.post._id
+            })
+        });
+        fetchData();
+    };
+
+    console.log(newCommentEmail)
+    console.log(newCommentText)
 
     return (
         <div className="min-h-full m-5">
@@ -31,7 +60,21 @@ export default function Post(props) {
                         <p>{postData.post.text}</p>
                     </div>
                     <div>
-                        <p>comments: </p>
+                        <p className="text-lg m-3">Leave a comment:</p>
+                        <form onSubmit={e => handleSubmit(e)}>
+                            <div className="m-3">
+                                <label className="m-3" htmlFor="email">Email:</label>
+                                <input className="text-black" onChange={e => setNewCommentEmail(e.target.value)} 
+                                type="email" name="email" id="email" value={newCommentEmail}/>    
+                            </div>
+                            <div className="m-3">
+                                <label className="m-3" htmlFor="comment">Your Comment:</label>
+                                <input className="text-black" onChange={e => setNewCommentText(e.target.value)} type="text" name="text" id="text" value={newCommentText} />
+                            </div>
+                            <input className="font-bold text-lg dark:bg-white dark:text-black
+                             px-4 py-2 m-3 rounded" type="submit" value="SEND" />
+                        </form>
+                        <p className="m-5 text-lg font-bold">Comments: </p>
                         {postData.comments.map(comment => {
                             return <div key={comment._id}>
                                 <p>{comment.user_email}: {comment.text}</p>

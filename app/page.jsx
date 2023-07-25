367aba95206d6899153bbf7890aa1e8f04a06f61
nagyb3 <nagybence2003@gmail.com>
2023-07-25 20:42:29 +0200

    add login and signup button

--- app/page.jsx
+++ app/page.jsx
@@ -23,7 +23,13 @@ export default function Home() {
 
   return (
     <main className="min-h-screen items-center p-5">
-      <h1 className='text-center font-bold text-2xl m-10'><a href="/">nagyb3's blog</a></h1>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-center font-bold text-2xl m-12'><a href="/">nagyb3's blog</a></h1>
+        <div>
+          <button className='h-min m-10 bg-white p-3 rounded text-black font-bold'><a href="/signup">Sign Up</a></button>
+          <button className='m-12 font-bold'><a href="/login">Log In</a></button>
+        </div>
+      </div>
       <p className='text-xl font-bold m-2'>ALL OF THE POSTS:</p>
       {allPosts.map(item => {
         return <div key={item._id} className='rounded-lg flex flex-col gap-10 border-2 dark:border-white border-black px-5 py-10 m-5'>

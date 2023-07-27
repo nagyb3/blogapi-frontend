fbe14ab4f2cde02a8ab9604ee08dab10b5fd01c9
nagyb3 <nagybence2003@gmail.com>
2023-07-27 16:13:08 +0200

    make react funciton name uppercase on /admin page.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-export default function page() {
+export default function Page() {
   const [isAdmin, setIsAdmin] = React.useState(true);
 
   React.useEffect(() => {
